65c740c1d95d3f8147d37201edf78a78a5c5b607
unknown <pierrick@live.co.uk>
2021-06-22 16:33:46 +0100

    more stuff

--- script.js
+++ script.js
@@ -1,90 +1,61 @@
-class Cube {
-    constructor(x, y, z) {
-        this.x = x
-        this.y = y
-        this.z = z
-        this.material = material
+var scene = new THREE.Scene()
+
+var camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000)
+camera.position.x = 10
+camera.position.y = 10
+camera.position.z = 15
+camera.rotation.y = 0.5
+camera.rotation.x = -0.5
+
+var renderer = new THREE.WebGLRenderer({antialias: true})
+renderer.setClearColor("#000000")
+renderer.setSize(window.innerWidth,window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth,window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight
+
+    camera.updateProjectionMatrix()
     }
+)
+
+chunk(8)
+light(0, 10, 10)
+
+render()
 
 
+
+
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
 
-var scene = new THREE.Scene();
+function cube(x, y, z, color) {
+    var geometry = new THREE.BoxGeometry(0.8, 0.8, 0.8)
+    var material = new THREE.MeshLambertMaterial({color: color})
+    var mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = x
+    mesh.position.y = y
+    mesh.position.z = z
+    scene.add(mesh)
+}
 
-    var camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000)
-    camera.position.z = 10;
-
-    var renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setClearColor("#010101");
-    renderer.setSize(window.innerWidth,window.innerHeight);
-
-    document.body.appendChild(renderer.domElement);
-
-    window.addEventListener('resize', () => {
-        renderer.setSize(window.innerWidth,window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-
-        camera.updateProjectionMatrix();
-    })
-
-    var raycaster = new THREE.Raycaster();
-    var mouse = new THREE.Vector2();
-
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshLambertMaterial({color: 0xffffff});
-    //var mesh = new THREE.Mesh(geometry, material);
-
-    //scene.add(mesh);
-
-    meshX = -10;
-    for(var i = 0; i < 255; i++) {
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.position.x = getRandomInt(-3, 3);
-        mesh.position.y = getRandomInt(-3, 3);
-        mesh.position.z = getRandomInt(-3, 3);
-        scene.add(mesh);
-        meshX+=1;
-    }
-
-
-    var light = new THREE.PointLight(0xFFFFFF, 1, 1000)
-    light.position.set(0,0,0);
-    scene.add(light);
-
-    var light = new THREE.PointLight(0xFFFFFF, 2, 1000)
-    light.position.set(0,0,25);
-    scene.add(light);
-
-    var render = function() {
-        requestAnimationFrame(render);
-
-
-        renderer.render(scene, camera);
-    }
-
-    function onMouseMove(event) {
-        event.preventDefault();
-
-        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-        raycaster.setFromCamera(mouse, camera);
-
-        var intersects = raycaster.intersectObjects(scene.children, true);
-        for (var i = 0; i < intersects.length; i++) {
-            this.tl = new TimelineMax();
-            this.tl.to(intersects[i].object.scale, 1, {x: 2, y: 2, z: 2, ease: Expo.easeOut})
-            this.tl.to(intersects[i].object.scale, .5, {x: .5, y: .5, z: .5, ease: Expo.easeOut})
-            this.tl.to(intersects[i].object.position, .5, {x: getRandomInt(3), y: getRandomInt(3), z: getRandomInt(3), ease: Expo.easeOut})
-            this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5")
+function chunk(size) {
+    for (x = 0; x < size; x++) {
+        for (y = 0; y < size; y++) {
+            for (z = 0; z < size; z++) {
+                cube(x, y, z)
+            }
         }
     }
+}
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-    }
-
-    window.addEventListener('mousemove', onMouseMove);
-    render();
+function light(x, y, z) {
+    var light = new THREE.PointLight(0xffffff, 1, 1000)
+    light.position.set(x, y, z);
+    scene.add(light);
+}
